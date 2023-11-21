eac047836fff2cf4b273b039508d6a54bda9f90b
harshgupta0 <hg09032@gmail.com>
2023-11-21 16:45:54 +0530

    added comments to explain code

--- lib/GetUserLoaction/Get_user_location.dart
+++ lib/GetUserLoaction/Get_user_location.dart
@@ -5,20 +5,27 @@ import 'package:geolocator/geolocator.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:task_hyginiee/places/Places.dart';
 
+// This widget is responsible for getting user location and displaying it on Google Maps.
 class GetUserLocation extends StatefulWidget {
   const GetUserLocation({Key? key}) : super(key: key);
-
   @override
   State<GetUserLocation> createState() => _GetUserLocationState();
 }
 
 class _GetUserLocationState extends State<GetUserLocation> {
+  // Completer to handle the GoogleMapController asynchronously.
   final Completer<GoogleMapController> _controller = Completer();
+
+  // Initial camera position when the map is loaded.
   static CameraPosition _initialPosition = CameraPosition(
     target: LatLng(28.65701173874114, 77.23689340564452),
     zoom: 15,
   );
+
+  // List of markers to be displayed on the map.
   final List<Marker> myMarker = [];
+
+  // Predefined list of markers (for demonstration purposes).
   final List<Marker> markerList = const [
     Marker(
       markerId: MarkerId("first"),
@@ -30,21 +37,28 @@ class _GetUserLocationState extends State<GetUserLocation> {
   @override
   void initState() {
     super.initState();
+    // Add predefined markers to myMarker list during initialization.
     myMarker.addAll(markerList);
   }
 
+  // Asynchronous function to get the user's current location using the Geolocator plugin.
   Future<Position> getUserLocation() async {
-    await Geolocator.requestPermission().then((value) {}).onError((error,
-        stackTrace) {
+    // Request permission to access device location.
+    await Geolocator.requestPermission().then((value) {}).onError((error, stackTrace) {
       print('error$error');
     });
+    // Return the current position.
     return Geolocator.getCurrentPosition();
   }
 
+  // Function to update the map with the user's current location.
   void packData() {
+    // Get the user's current location using Geolocator.
     Geolocator.getCurrentPosition().then((value) async {
       print("my Location");
       print("${value.latitude}${value.longitude}");
+
+      // Add a marker for the user's current location to the myMarker list.
       myMarker.add(
         Marker(
           markerId: MarkerId('Second position'),
@@ -52,12 +66,18 @@ class _GetUserLocationState extends State<GetUserLocation> {
           infoWindow: InfoWindow(title: "My location"),
         ),
       );
+
+      // Set the camera position to focus on the user's current location.
       CameraPosition cameraPosition = CameraPosition(
         target: LatLng(value.latitude, value.longitude),
         zoom: 15,
       );
+
+      // Get the GoogleMapController from the Completer and animate the camera to the new position.
       final GoogleMapController controller = await _controller.future;
       controller.animateCamera(CameraUpdate.newCameraPosition(cameraPosition));
+
+      // Trigger a rebuild to update the UI.
       setState(() {});
     });
   }
@@ -69,10 +89,12 @@ class _GetUserLocationState extends State<GetUserLocation> {
         appBar: AppBar(
           title: Text(
             "MAP",
-            style: TextStyle(fontWeight: FontWeight.bold,
-                color: Colors.blue,
-                letterSpacing: 2,
-                fontSize: 20),
+            style: TextStyle(
+              fontWeight: FontWeight.bold,
+              color: Colors.blue,
+              letterSpacing: 2,
+              fontSize: 20,
+            ),
           ),
           centerTitle: true,
           backgroundColor: Colors.blue.shade200.withOpacity(.4),
@@ -80,21 +102,21 @@ class _GetUserLocationState extends State<GetUserLocation> {
             IconButton(
               icon: Icon(Icons.location_on),
               onPressed: () {
+                // When location icon is pressed, update the map with the user's current location.
                 packData();
               },
             ),
             IconButton(
               icon: Icon(Icons.search),
               onPressed: () {
-                Navigator.push(context, MaterialPageRoute(builder: (context)=>Places()));
-
+                // Navigate to the Places screen when the search icon is pressed.
+                Navigator.push(context, MaterialPageRoute(builder: (context) => Places()));
               },
             ),
             PopupMenuButton<String>(
-
+              // Create a popup menu with profile, history, and share location options.
               itemBuilder: (BuildContext context) {
-                return {'profile', 'History', 'Share Loaction'}.map((
-                    String choice) {
+                return {'profile', 'History', 'Share Location'}.map((String choice) {
                   return PopupMenuItem<String>(
                     value: choice,
                     child: Text(choice),
@@ -102,26 +124,30 @@ class _GetUserLocationState extends State<GetUserLocation> {
                 }).toList();
               },
               onSelected: (String choice) {
-                // Handle menu item selection
+                // Handle menu item selection if needed.
               },
             ),
           ],
         ),
+        // Floating action button to manually update the user's location on the map.
         floatingActionButton: Container(
           margin: EdgeInsets.only(bottom: 25),
           child: FloatingActionButton(
             backgroundColor: Colors.blue.shade200.withOpacity(.7),
             onPressed: () {
+              // When the floating action button is pressed, update the map with the user's current location.
               packData();
             },
             child: Icon(Icons.radio_button_off),
           ),
         ),
         floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+        // GoogleMap widget to display the map.
         body: GoogleMap(
           initialCameraPosition: _initialPosition,
           markers: Set<Marker>.of(myMarker),
           onMapCreated: (GoogleMapController controller) {
+            // Callback function when the map is created, providing the GoogleMapController.
             _controller.complete(controller);
           },
         ),
--- lib/places/Places.dart
+++ lib/places/Places.dart
@@ -3,6 +3,8 @@ import 'package:geocoding/geocoding.dart';
 import 'package:http/http.dart' as http;
 import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
+
+// A StatefulWidget for displaying a list of places and searching for places.
 class Places extends StatefulWidget {
   const Places({super.key});
 
@@ -11,43 +13,68 @@ class Places extends StatefulWidget {
 }
 
 class _PlacesState extends State<Places> {
+  // Controller for the search input field.
   TextEditingController _controller = TextEditingController();
-  String  tokenForSession ="34765";
-  var uuid=Uuid();
-  List<dynamic> ListForPlaces =[];
-  void makeSuggestions (String input) async {
-    String googlePlacesApikey ="AIzaSyANU53A5MDj36XQWxtVtMVuQjFE-JB6IUY";
-    String groundURL ='https://maps.googleapis.com/maps/api/place/autocomplete/json';
+
+  // Initial session token for Google Places API.
+  String tokenForSession = "34765";
+
+  // Uuid instance for generating unique session tokens.
+  var uuid = Uuid();
+
+  // List to store suggestions/places received from the Google Places API.
+  List<dynamic> ListForPlaces = [];
+
+  // Function to fetch place suggestions from the Google Places API.
+  void makeSuggestions(String input) async {
+    // Google Places API key for authentication.
+    String googlePlacesApikey = "AIzaSyANU53A5MDj36XQWxtVtMVuQjFE-JB6IUY";
+
+    // Google Places API endpoint for autocomplete predictions.
+    String groundURL = 'https://maps.googleapis.com/maps/api/place/autocomplete/json';
+
+    // Build the API request URL.
     String request = '$groundURL?input=$input&key=$googlePlacesApikey&sessiontoken=$tokenForSession';
 
-    var ResponseResult =await http.get(Uri.parse(request));
-    var ResultData= ResponseResult.body.toString();
+    // Make a GET request to the Google Places API.
+    var ResponseResult = await http.get(Uri.parse(request));
+    var ResultData = ResponseResult.body.toString();
     print("ResultData");
     print(ResultData);
-    if(ResponseResult.statusCode==200){
+
+    // Check if the request was successful (status code 200).
+    if (ResponseResult.statusCode == 200) {
+      // Update the ListForPlaces with the suggestions received from the API response.
       setState(() {
-      ListForPlaces = jsonDecode(ResponseResult.body.toString())['predictions'];
+        ListForPlaces = jsonDecode(ResponseResult.body.toString())['predictions'];
       });
-    }else{
-      throw Exception('showing data failed , try again');
+    } else {
+      // Throw an exception if the request was not successful.
+      throw Exception('Showing data failed, try again');
     }
   }
-  void OnModify(){
-    if(tokenForSession==null){
+
+  // Function to handle modifications in the search input.
+  void OnModify() {
+    // If the session token is null, generate a new one.
+    if (tokenForSession == null) {
       setState(() {
-       tokenForSession =uuid.v4();
+        tokenForSession = uuid.v4();
       });
     }
+    // Call makeSuggestions with the current input.
     makeSuggestions(_controller.text);
   }
+
   @override
   void initState() {
-    // TODO: implement initState
     super.initState();
+    // Add a listener to the search input to trigger OnModify on text changes.
     _controller.addListener(() {
       OnModify();
     });
   }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
@@ -56,7 +83,7 @@ class _PlacesState extends State<Places> {
           gradient: LinearGradient(
             begin: Alignment.topLeft,
             end: Alignment.bottomRight,
-            colors: [Colors.blue.shade600,Colors.blue.shade400,Colors.blue.shade200],
+            colors: [Colors.blue.shade600, Colors.blue.shade400, Colors.blue.shade200],
           ),
         ),
         child: Scaffold(
@@ -92,21 +119,23 @@ class _PlacesState extends State<Places> {
                     style: TextStyle(color: Colors.white),
                   ),
                 ),
-                Expanded(child: ListView.builder(
+                Expanded(
+                  child: ListView.builder(
                     itemCount: ListForPlaces.length,
-                    itemBuilder: (context , index){
-                    return ListTile(
-                      onTap: ()
-                      async{
-                        List<Location> location = await locationFromAddress(ListForPlaces[index]['description']);
-                        // List<Location> location = await locationFromAddress
-                        print(location.last.latitude);
-                        print(location.last.longitude);
-                      },
-                      title: Text(ListForPlaces[index]['description']),
-                    );
-
-                })),
+                    itemBuilder: (context, index) {
+                      return ListTile(
+                        onTap: () async {
+                          // Get the location details (latitude, longitude) from the selected place.
+                          List<Location> location =
+                          await locationFromAddress(ListForPlaces[index]['description']);
+                          print(location.last.latitude);
+                          print(location.last.longitude);
+                        },
+                        title: Text(ListForPlaces[index]['description']),
+                      );
+                    },
+                  ),
+                ),
               ],
             ),
           ),
